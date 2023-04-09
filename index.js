59e5bf293f57cf4f70a9aebf0fe81314b98dbd01
nepsoul <nirumagar901@gmail.com>
2023-04-09 21:14:56 +0545

    chore: use of direct sql with sequelize method query into blog app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize, QueryTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
@@ -12,35 +12,47 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    author: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  }
-);
+// class Blog extends Model {}
+// Blog.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     title: {
+//       type: DataTypes.TEXT,
+//       allowNull: false,
+//     },
+//     url: {
+//       type: DataTypes.TEXT,
+//       allowNull: false,
+//     },
+//     author: {
+//       type: DataTypes.TEXT,
+//       allowNull: false,
+//     },
+//     likes: {
+//       type: DataTypes.TEXT,
+//       allowNull: true,
+//     },
+//   },
+//   {
+//     sequelize,
+//     underscored: true,
+//     timestamps: false,
+//     modelName: "blog",
+//   }
+// );
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await sequelize.query("select* from blogs", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(blogs);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
